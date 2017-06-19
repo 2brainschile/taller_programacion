55afecc760dd3c46f997d1c8a8549ff093fb0565
Sebastián Schuchhardt <seba.sch999@gmail.com>
2017-06-19 11:13:47 -0400

    1era Clase Practica: Conceptos basicos en JS :muscle:

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# taller_programacion
-Taller de Programacion 2Brains
+# Taller de Programacion 2Brains
+
+## Para correr ej:
+- Hacer doble click en el archivo *index.html* o abrirlo en algun navegador
+- Editar con Sublime u otro editor el archivo *file.js*
+
---
+++ file.js
@@ -0,0 +1,51 @@
+//*********************************
+// Código Javascript
+//*********************************
+console.log("JS")
+///////////////////////////////
+///////////////////////////////
+
+// Operaciones definidas
+// sumar = 1 + 1
+// resta = 1 - 1
+// multiplicacion = 1 * 1
+// division = 1 / 1
+
+// Función que muestra la suma de dos números
+function sumar(numero1, numero2) {
+	var resultado = numero1 + numero2;
+	console.log(resultado);
+};
+
+// EJ:
+// sumar(5, 5) // Muestra 10
+// sumar(123, 2) // Muestra 125
+
+
+// Función que muestra la resta de dos números
+function restar(numero1, numero2) {
+	var resultado = numero1 - numero2;
+	console.log(resultado);
+};
+
+// EJ:
+// restar(5, 5) // Muestra 0
+// restar(123, 2) // Muestra 121
+
+
+//*********************************
+// TAREA
+//*********************************
+// Crear funcion calculadora, que tenga las dos funciones: SUMAR Y RESTAR
+//
+
+function calculadora(operacion, numero1, numero2) {
+	// Código de Tarea
+}
+
+//EJ: 
+calculadora("sumar", 4, 5) // -> Deberia mostrar 9
+calculadora("restar", 4, 3)// -> Deberia mostrar 1
+
+
+
